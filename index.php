387310cebb9fcf026d4bbe24722e903bb5d5e319
msangiasia <Msangiasia61@gmail.com>
2022-10-22 00:40:54 -0700

    first commit

--- index.php
+++ index.php
@@ -7,12 +7,25 @@
 
 </head>
 <body>
-<style></style>
+<style>
+	@font-face{
+		
+	}
+</style>
 <header>
 	<div class="logo-header">
-		<img src="assets/images">
+		<img src="assets/images/icon/W.jpg">
 	</div>
-	<div></div>
+	<div class="header-div">
+		<div class="main-title">SKIA MUSIC</div>
+		<div class="main-nav"><a href="">Home</div></a>
+		<div class=" nav-item"><a href="">Musics</div></a>
+		<div class=" nav-item"><a href="">category</div></a>
+		<div class=" nav-item"><a href="">about us</div></a>
+		<div class=" nav-item"><a href="">contact us</div></a>
+		<div class=" nav-item"><a href="">Hi, user</div></a>
+		
+	</div>
 </header>
 </body>
 </html>
