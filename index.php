738fce3423f4409c8cca94ae18f46a1a8f973bbc
msangiasia <Msangiasia61@gmail.com>
2022-10-23 23:31:01 -0700

    first commit

--- index.php
+++ index.php
@@ -11,26 +11,70 @@
 	*{
 		box-sizing: border-box;
 
+	
 	}
-	body{
+	body a{
       text-decoration: none;
       margin: 0px;
       padding: 0px;
 	}
+	header{
+		display: flex;
+
+	}
+	.logo-holder{
+		max-width:150px;
+        flex:1;
+	}
+    body img{
+    	width: 50%; 
+    }
+    .header-div{
+         flex: auto;
+
+    }
+     .main-nav{
+     	display: flex;
+     }
+    .main-item{
+    	padding: 10px;
+    	text-align:left;
+    	
+    }
+    .main-nav{
+    	padding: 10px;
+    	text-align:right;
+    }
+    .main-title{
+    	padding: 10px;
+    }
+     h1{
+     	text-align: center;
+     	background-color:#3390FF;
+     }
+     
+     .main-title{
+     	padding: 10px;
+
+     }
+     h1 .active{
+     	border-bottom: solid 4px red;
+     }
 </style>
 <header>
 	<div class="logo-header">
-		<img src="assets/images/icon/W.jpg">
+	
 	</div>
 	<div class="header-div">
-		<div class="main-title">SKIA MUSIC</div>
-		<div class="main-nav"><a href="">Home</div></a>
-		<div class=" nav-item"><a href="">Musics</div></a>
-		<div class=" nav-item"><a href="">category</div></a>
-		<div class=" nav-item"><a href="">about us</div></a>
-		<div class=" nav-item"><a href="">contact us</div></a>
-		<div class=" nav-item"><a href="">Hi, user</div></a>
+		<div class="main-title"><h1>SKIA MUSIC</h1></div>
+		<div class="main-btn">
+		<div class="main-item active"><a href="home">Home</div></a>
+		<div class=" main-nav"><a href="">Musics</div></a>
+		<div class=" main-nav"><a href="">Playlist</div></a>
+		<div class=" main-nav"><a href="">Genre</div></a>
 		
+		<div class=" main-nav"><a href=""> users</div></a>
+		</div>
 	</div>
 </header>
 </body>
